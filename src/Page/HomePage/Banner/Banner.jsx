c6f2668372e129c4e2519177ee2631db344deddf
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-08-20 16:00:51 +0600

    add sponsored logo

--- src/Page/HomePage/Banner/Banner.jsx
+++ src/Page/HomePage/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className="banner-img h-screen w-full text-white flex justify-center items-center">
       <div className="md:w-10/12 w-11/12">
         <h1 className="md:text-6xl text-3xl font-bold ">Black Mesa</h1>
-        <p className="text-xl my-7">
+        <p className="text-xl my-7 md:leading-normal leading-10">
           “Have beginning wherein spirit. Bearing unto. To after light. Good
           image can’t without hath <br /> fly beast him fish firmament fish Face
           waters fly unto spirit i doesn’t one waters divide.”
